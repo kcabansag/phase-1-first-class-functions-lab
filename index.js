f3f668e2d1e756713555f691f87c6eb11391161a
Kris Janssen Cabansag <kjs.cabansag@gmail.com>
2024-02-22 11:57:51 -0500

    complete advance first class functions lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+
+ const returnFirstTwoDrivers = (scubersDrivers) => {
+    return scubersDrivers.slice(0, 2);
+ }
+
+
+ const returnLastTwoDrivers = (scubersDrivers) => {
+    return scubersDrivers.slice(-2);
+ }
+ 
+
+ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+
+ const createFareMultiplier = (fareMultiplier) => {
+    return (fare) => {
+        return fare * fareMultiplier;
+    }
+ }
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (scubersDrivers, driverSelector) => {
+    return driverSelector(scubersDrivers);
+}
